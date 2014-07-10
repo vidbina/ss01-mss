dd6758ece2936b3e883ac65851d1e274fa4d2836
David Asabina <david@supr.nu>
2014-07-10 16:20:27 +0200

    Plotting output from both sensors for different angles and post-feedback values

--- sensor_adc.m
+++ sensor_adc.m
@@ -1,24 +1,25 @@
 % A description of the conversion of the sensor output witnessed at the ADC
 % input to whichever metric is required.
-function retval = angleToVoltage(angle, supply=3.3, sensor=1)
+function [a, b] = angleToVoltage(angle, supply=3.3)
+  a = 12e-3*supply*sin(2*angle*pi/180);
+  b = 12e-3*supply*cos(2*angle*pi/180);
+endfunction
+
+function [a, b] = angleToADC(angle, supply=3.3, gain=33, offset=3.3/2)
+  [a, b] = angleToVoltage(angle, supply); %*gain + offset);
+  a = a*gain+offset;
+  b = b*gain+offset;
+endfunction
+
+% signal is equal to the voltage measured by the ADC
+function retval = levelToAngle(signal, supply=3.3, gain=33, offset=3.3/2, sensor=1)
   if sensor == 1
-    retval = 12e-3*supply*sin(2*angle*pi/180);
+    retval = 0.5*asin((signal-offset)/(12e-3*supply*gain))*180/pi;
   elseif sensor == 2
-    retval = 12e-3*supply*cos(2*angle*pi/180);
+    retval = 0.5*acos((signal-offset)/(12e-3*supply*gain))*180/pi;
   else
     retval = NA
   endif
-endfunction
-
-function retval = angleToADC(angle, supply=3.3, gain=33, offset=3.3/2, sensor=1)
-  retval = (angleToVoltage(angle, supply, sensor)*gain + offset);
-endfunction
-
-% only this the conversion helpers within the specified ranges
-% signal should not be greater then the supply
-function retval = levelToAngle(signal, supply=3.3, gain=33, offset=3.3/2)
-  retval = 0.5*asin((signal-offset)/(12e-3*supply*gain))*180/pi;
-  printf(sprintf("arcsin(%f/%f)=%f\n\r", (signal-offset), (12e-3*supply*gain), retval*2));
 endfunction
 
 function retval = ratioToAngle(ratio, supply=3.3, gain=33, offset=3.3/2)
@@ -46,30 +47,34 @@ function retval = valueBtoAngle(value)
   retval = valueToAngle(value, 3465, 986);
 endfunction
 
-range = 0:360;
+range = 0:180;
 supply = 3.3;
 gain = 33;
 
 figure(1);
-a_out = arrayfun(@(x) angleToVoltage(x, 3.3, 1), range);
-b_out = arrayfun(@(x) angleToVoltage(x, 3.3, 2), range);
+[a_out, b_out] = arrayfun(@(x) angleToVoltage(x, 3.3), range);
 plot(range, a_out, range, b_out);
 xlabel('rotation in degrees');
 ylabel('expected output in volts');
 legend('A', 'B');
-title("Sensor output")
+title("Sensor output");
 
 figure(2);
-a_adc = arrayfun(@(x) angleToADC(x, supply, gain, supply/2, 1), range);
-b_adc = arrayfun(@(x) angleToADC(x, supply, gain, supply/2, 2), range);
+%a_adc = arrayfun(@(x) angleToADC(x, supply, gain, supply/2, 1), range);
+%b_adc = arrayfun(@(x) angleToADC(x, supply, gain, supply/2, 2), range);
+[a_adc, b_adc] = arrayfun(@(x) angleToADC(x), range);
 plot(range, a_adc, range, b_adc);
 xlabel('rotation in degrees');
 ylabel('expected output in volts');
 legend('A', 'B');
 title("Amplifier output");
 
-%%figure(3);
-%%plot(arrayfun(@(x) levelToAngle(x), arrayfun(@(x) angleToVoltage(x), 0:360)));
-%%title("Angle to voltage conversion and then back to angle");
-%
-%%arcsin(0.56)*0.5 = -10
+figure(3);
+[a_adc, b_adc] = arrayfun(@(x) angleToADC(x), range);
+a_returned = arrayfun(@(x) levelToAngle(x, supply, gain, supply/2, 1), a_adc);
+b_returned = arrayfun(@(x) levelToAngle(x, supply, gain, supply/2, 2), b_adc);
+plot(range, range, range, a_returned, range, b_returned);
+legend('actual angle', 'feedback from A', 'feedback from B');
+xlabel('rotation in degrees');
+ylabel('readout in degrees');
+title("Angular feedback from sensors over 360 degree range");
